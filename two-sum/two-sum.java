8ef6426d68c31af8aed0d689cd5dd2cba8e1704c
Faris Qadan <79219452+farisqadan@users.noreply.github.com>
2021-11-10 20:10:12 -0500

    Time: 3 ms (68.18%), Space: 40.5 MB (37.78%) - LeetHub

---
+++ two-sum/two-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    // O(n^2) time
+    // O(1) space
+    /*public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] temp = {i, j};
+                    return temp;
+                }
+            }
+        }
+        return new int[2];
+    }
+    */
+    // O(n) space and time complexity
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i, map.get(complement)};
+            }
+        }
+        return null;
+    }
+}
