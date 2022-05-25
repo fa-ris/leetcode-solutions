b46554065d88810ca8f5862a4916676d2462b6c9
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-05-25 19:08:14 +0400

    Time: 1 ms (52.00%), Space: 41.8 MB (41.00%) - LeetHub

--- 1085-sum-of-digits-in-the-minimum-number/1085-sum-of-digits-in-the-minimum-number.java
+++ 1085-sum-of-digits-in-the-minimum-number/1085-sum-of-digits-in-the-minimum-number.java
@@ -1,10 +1,14 @@
 class Solution {
     public int sumOfDigits(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[0] < 10) {
-            return nums[0] % 2 == 1 ? 0 : 1;
-        } else if (nums[0] < 100) {
-            return ((int) (nums[0] / 10) + nums[0] % 10) % 2 == 1 ? 0 : 1;
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        //Arrays.sort(nums);
+        if (min < 10) {
+            return min % 2 == 1 ? 0 : 1;
+        } else if (min < 100) {
+            return ((int) (min / 10) + min % 10) % 2 == 1 ? 0 : 1;
         }
         return 0;
     }
