36b2d1855afe36da3f7b90e5c6ee9de20af53eee
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-05-25 18:45:14 +0400

    Time: 0 ms (100.00%), Space: 38.9 MB (97.39%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        if (num == 0) {
-            return true;
-        }
-        return !(num % 10 == 0);
+        return !(num % 10 == 0) || num == 0;
     }
 }
