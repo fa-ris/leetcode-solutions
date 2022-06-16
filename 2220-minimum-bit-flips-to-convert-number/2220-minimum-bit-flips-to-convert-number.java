d441b3b4700ac805ba53db2bd5f13a63619f734a
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-06-15 20:43:23 -0400

    Time: 1 ms (73.45%), Space: 41.6 MB (17.44%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,29 +1,5 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        char[] s = Integer.toBinaryString(start).toCharArray();
-        char[] g = Integer.toBinaryString(goal).toCharArray();
-        int i = s.length - 1;
-        int j = g.length - 1;
-        int count = 0;
-        while (i >= 0 && j >= 0) {
-            if (s[i] != g[j]) {
-                count++;
-            }
-            i--;
-            j--;
-        }
-        while (i >= 0) {
-            if (s[i] == '1') {
-                count++;
-            }
-            i--;
-        }
-        while (j >= 0) {
-            if (g[j] == '1') {
-                count++;
-            }
-            j--;
-        }
-        return count;
+        return Integer.bitCount(start ^ goal);
     }
 }
