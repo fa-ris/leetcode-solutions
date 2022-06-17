578dd3e42367aae6756644d0f085d594dfb8052f
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-06-17 17:23:45 -0400

    Time: 5 ms (11.83%), Space: 42 MB (11.42%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n > 0 && Integer.bitCount(n) == 1;
+        return n > 0 && Integer.toBinaryString(n).length() - Integer.toBinaryString(n).replace("1", "").length() == 1;
     }
 }
