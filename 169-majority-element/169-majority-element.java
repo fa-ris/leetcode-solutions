91fa7dc290f3ad650bcb73acc0572d79f44b73bd
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-06-17 16:47:13 -0400

    Time: 16 ms (23.46%), Space: 55.6 MB (62.48%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.replace(nums[i], map.get(nums[i]) + 1);
+                if (map.get(nums[i]) > nums.length / 2) {
+                    return nums[i];
+                }
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        return -1;
     }
 }
