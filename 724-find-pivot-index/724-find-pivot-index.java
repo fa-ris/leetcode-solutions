d76e138a505c315bde0727ec5549ecc3d8379403
faris qadan <79219452+farisqadan@users.noreply.github.com>
2022-06-29 21:42:19 -0400

    Time: 3 ms (33.74%), Space: 53.8 MB (15.16%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] arr1 = new int[nums.length];
+        int[] arr2 = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                arr1[i] = nums[i];
+                arr2[nums.length - 1] = nums[nums.length - 1 - i];
+            } else {
+                arr1[i] = arr1[i - 1] + nums[i];
+                arr2[nums.length - 1 - i] = arr2[nums.length - i] + nums[nums.length - 1 - i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (arr1[i] == arr2[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
