6a2ac8c28f735696fb32ac78ab397b579bb92e41
Faris Qadan <79219452+fa-ris@users.noreply.github.com>
2024-08-17 12:59:39 +0200

    Time: 58 ms (69.88%), Space: 17.7 MB (53.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         m = {}
         for i in range(len(nums)):
-            if (target - nums[i]) in m:
+            if target - nums[i] in m.keys():
                 return [i, m[target - nums[i]]]
             m[nums[i]] = i
         return [-1, -1]
